20899a43f5c26f9cdd0fff6e87b39f99c13b3059
scott <remsyk@gmail.com>
2020-09-01 22:40:40 -0700

    still need to set up timers

--- app/src/main/java/com/example/mtg_commander_timer/MainFragment.kt
+++ app/src/main/java/com/example/mtg_commander_timer/MainFragment.kt
@@ -31,10 +31,28 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        timerList = arrayListOf(TimerModel("Enter name",0,null,true) ,TimerModel("name",0,null,true))
+        timerList =
+            arrayListOf(TimerModel("Enter name", 0, null, true), TimerModel("name", 0, null, true))
 
         picker_player_num.maxValue = 4
-        picker_player_num.minValue = 2
+        picker_player_num.minValue = timerList.size
+
+        include_enter_name_1.textLayout_name.hint = timerList.get(0).name
+        include_enter_name_2.textLayout_name.hint = timerList.get(1).name
+
+        if (timerList.size >2) {
+            include_enter_name_3.textLayout_name.hint = timerList.get(2).name
+            include_enter_name_4.textLayout_name.hint = timerList.get(3).name
+        }
+
+        include_enter_name_1.button_timer.text = timerList.get(0).countdownTime.millisToString()
+        include_enter_name_2.button_timer.text = timerList.get(1).countdownTime.millisToString()
+
+        if (timerList.size >2) {
+            include_enter_name_3.button_timer.text = timerList.get(2).countdownTime.millisToString()
+            include_enter_name_4.button_timer.text = timerList.get(3).countdownTime.millisToString()
+        }
+
 
         include_enter_name_1.textinputeditText_name.setOnFocusChangeListener { view, b ->
             if (!b) {
@@ -68,10 +86,11 @@ class MainFragment : Fragment() {
                 3 -> {
                     include_enter_name_3.visibility = View.VISIBLE
                     include_enter_name_4.visibility = View.GONE
-                    if(timerList.size<=2) {
-                        timerList.add(TimerModel("Enter Name", 0, null,true))
+                    if (timerList.size <= 2) {
+                        timerList.add(TimerModel("Enter Name", 0, null, true))
+
                     }
-                    if (timerList.size == 4){
+                    if (timerList.size == 4) {
                         timerList.removeLast()
                     }
                 }
@@ -79,13 +98,14 @@ class MainFragment : Fragment() {
                 4 -> {
                     include_enter_name_3.visibility = View.VISIBLE
                     include_enter_name_4.visibility = View.VISIBLE
-                    if(timerList.size<=3) {
-                        timerList.add(TimerModel("Enter Name", 0, null,true))
+                    if (timerList.size <= 3) {
+                        timerList.add(TimerModel("Enter Name", 0, null, true))
+
                     }
                 }
 
                 else -> {
-                    if(timerList.size == 3) {
+                    if (timerList.size == 3) {
                         timerList.removeLast()
                         include_enter_name_3.visibility = View.GONE
                         include_enter_name_4.visibility = View.GONE
